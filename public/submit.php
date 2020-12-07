667cfc975a7dcb3cec87be65d017d2e118638f00
Ben <benmason1992@hotmail.co.uk>
2020-12-07 22:44:06 +0000

    added IP address

--- public/submit.php
+++ public/submit.php
@@ -11,6 +11,7 @@ $message = $_POST['message'];
 $token = $_POST['token'];
 $newsletter = $_POST['newsletter'];
 $dateNow = date('Y-m-d');
+$ip = $_SERVER['REMOTE_ADDR'];
 
 //phone number
 $phonePattern = "/^(?:0|\+?44)(?:\d\s?){9,10}$/";
@@ -19,11 +20,14 @@ $match = preg_match($phonePattern, $phone);
 //email
 $to = "admin@test.com";
 $subject = "Form submission";
-$emailMessage = $name . " wrote the following:" . "\n\n" . $message . "\n\n" . "Would I like to sign up to the newsletter? " . $newsletter . "\n\n" . "This message was sent on " . $dateNow;
+$emailMessage = $name . " wrote the following:" . "\n\n" . $message . "\n\n" . "Would I like to sign up to the newsletter? " . $newsletter . "\n\n" . "This message was sent on " . $dateNow . ", from the following IP address " . $ip;
 $headers = "From:" . $email;
 
 //message length
 $messageLength = strlen($message);
+
+var_dump($emailMessage);
+die();
 
 //connection
 $connection = new Dbh;
